c7cf40b9336ab54d4377bbdc6e9860d11968ab60
Bob Hosten <bob.hostern@gmail.com>
2013-11-26 06:48:03 -0600

    PUpdate 0.2 - Mod DisandriaApp

--- src/lib/DisandriaApp.cpp
+++ src/lib/DisandriaApp.cpp
@@ -7,18 +7,33 @@ void DisandriaApp::initialize(Application& self)
 	Application::initialize(self);
 }
 
-void DisandriaApp::reinitialize()
+void DisandriaApp::reinitialize(Application& self)
 {
+	
+}
+
+const char* DisandriaApp::name()
+{
+	return "Disandria";
 }
 
 void DisandriaApp::uninitialize()
 {
 }
 
-void DisandriaApp::defineOptions(OptionSet&)
+void DisandriaApp::defineOptions(OptionSet& opst)
 {
+	Option version("version", "v", "Display current Disandria version", false);
+	version.callback(OptionCallback<DisandriaApp>(this, &DisandriaApp::sayVersion));
+	opst.addOption(version);
 }
 
 int DisandriaApp::main(const std::vector<std::string>& args)
 {
+}
+
+void DisandriaApp::sayVersion(const std::string& name, const std::string& value)
+{
+	this->logger().notice("Disandria v.0.1");
+	stopOptionProcessing();
 }
--- src/lib/DisandriaApp.h
+++ src/lib/DisandriaApp.h
@@ -6,10 +6,12 @@
 class DisandriaApp : public Poco::Util::Application
 {
 	void initialize(Poco::Util::Application&);
-	void reinitialize();
+	void reinitialize(Poco::Util::Application& self);
 	void uninitialize();
 	void defineOptions(Poco::Util::OptionSet&);
 	int main(const std::vector<std::string>& args);
+	const char* name();
+	void sayVersion(const std::string&, const std::string&);
 };
 
 #endif // __DISANDRIAAPP_DISANDRIA_H
